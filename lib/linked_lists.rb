5ac8600b314bb6d5ae2a119b4f0286dc91331266
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-05-17 20:23:38 +0200

    Set up classes

---
+++ lib/linked_lists.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# This is the linked list data structure
+class LinkedList
+  def append(value); end
+
+  def prepend(value); end
+
+  def size; end
+
+  def head; end
+
+  def tail; end
+
+  def at(index); end
+
+  def pop; end
+
+  def contains?(value); end
+
+  def find(value); end
+
+  def to_s; end
+end
+
+# This is a simple node
+class Node
+  def value
+    nil
+  end
+
+  def next_node
+    nil
+  end
+end
